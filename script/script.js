4a1f64622ec5a1a94fda73b628103d67a49c5be7
dirk-woehr <51853046+dirk-woehr@users.noreply.github.com>
2024-06-27 01:31:13 +0200

    Base setup

---
+++ script/script.js
@@ -0,0 +1,28 @@
+const types = ["Essen", "Konzerte", "Sonstiges"];
+const data = {
+  Essen: [
+    {
+      name: "Name",
+      japaneseName: "名前",
+      link: "#",
+      googleMaps: "#",
+      date: "2024-11-30",
+    },
+    {
+      name: "Other Name",
+      link: "#",
+      googleMaps: "#",
+      date: "2024-12-01",
+    },
+    {
+      name: "Yet Another Name",
+      link: "#",
+      date: "2024-12-01",
+    },
+    {
+      name: "Final Name",
+      link: "#",
+      googleMaps: "#",
+    },
+  ],
+};
